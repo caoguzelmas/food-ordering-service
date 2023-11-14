705b791f9679633bb1e5db106486c8ff438e6903
caoguzelmas <caoguzelmas@gmail.com>
2023-11-15 00:15:51 +0100

    kafka consumer & producer created

---
+++ infrastructure/kafka/kafka-consumer/src/main/java/com/caoguzelmas/foodorderingservice/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,10 @@
+package com.caoguzelmas.foodorderingservice.kafka.consumer;
+
+import org.apache.avro.specific.SpecificRecordBase;
+
+import java.util.List;
+
+public interface KafkaConsumer<T extends SpecificRecordBase> {
+
+    void receive(List<T> message, List<Long> keys, List<Integer> partitions, List<Long> offsets);
+}
---
+++ infrastructure/kafka/kafka-producer/src/main/java/com/caoguzelmas/foodorderingservice/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,8 @@
+package com.caoguzelmas.foodorderingservice.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
