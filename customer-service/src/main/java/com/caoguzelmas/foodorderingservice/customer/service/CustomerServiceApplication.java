4c9245eb1ec90fb8864925bb28a7a10e61387f9d
caoguzelmas <caoguzelmas@gmail.com>
2023-11-19 23:27:38 +0100

    customer-service postgres connection implemented

---
+++ customer-service/src/main/java/com/caoguzelmas/foodorderingservice/customer/service/CustomerServiceApplication.java
@@ -0,0 +1,11 @@
+package com.caoguzelmas.foodorderingservice.customer.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.caoguzelmas.foodorderingservice")
+public class CustomerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+}
